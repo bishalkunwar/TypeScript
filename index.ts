9c875e5ea6ed12f325ec894b1c3e2a635af11007
Bishal487 <bishal.kunwar487@gmail.com>
2024-03-29 14:58:52 -0400

    variables types in ts

--- index.ts
+++ index.ts
@@ -1,5 +1,17 @@
+
+// Getting started with TS.
+
 let myString: string;
-
 myString = "Hello from TS";
-
 console.log(myString);
+
+
+// variables declaration in TS.
+var namee: string = "Bishal";
+var salary: number = 1234;
+var married: boolean = false;
+var hours: number[]= [23, 34, 56];
+var friends: string[] = ["Bishal", "Suzan"];
+var mixed: (number|string)[] = ["bishal", 1, "ram", 2]
+
+console.log(` ${namee} \n ${salary}\n ${married}\n ${hours}\n ${friends}\n ${mixed}`)
