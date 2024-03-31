35e89b1a9284f2231a3aa34fca1d30dae1fcdbb2
Bishal487 <bishal.kunwar487@gmail.com>
2024-03-31 17:36:18 -0400

    passing objects as function paramaters in TS

---
+++ objects.ts
@@ -0,0 +1,18 @@
+// example showing the objects example using TS
+
+let employeeDetails = {
+    empName: "bishal",
+    empSection: "science"
+};
+
+console.log(`Emp Name \t Emp Section`);
+console.log(`${employeeDetails.empName} \t ${employeeDetails.empSection}`);
+
+
+function display( obj: {
+    empName:String, empSection:String
+}): void{
+    console.log(`FName is ${obj.empName} LName is ${obj.empSection}`);
+};
+
+display(employeeDetails);
