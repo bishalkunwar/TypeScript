1b0e20171bea03244a610ab46ff0825220698da7
Bishal487 <bishal.kunwar487@gmail.com>
2024-03-31 19:18:27 -0400

    type assertion with variabl tfype and object type too.

--- typeAssertion.ts
+++ typeAssertion.ts
@@ -7,3 +7,26 @@ console.log(`${str} of type ${typeof(str)}`);
 // let len:number = str.length; // this will give the error as str is type unknown so we cannot get the length
 let len:number = (str as string).length; 
 console.log(len);
+
+
+// type assertion using <> angle brackets syntax.
+let num: any = 69;
+
+let num1 = <Number> num;  // conversion from any to number.
+console.log(num1);
+console.log(typeof num1);
+
+
+// below is an example of type assertion with objects
+interface studDetails{
+    fName:string;
+    lName: string;
+}
+
+let person = <studDetails>{};
+
+person.fName = "bishal";
+person.lName = "kunwar";
+
+console.log(`${person.fName} \t ${person.lName}`);
+
