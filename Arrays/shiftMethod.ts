d8d8d9fb219200b815e444b74c8b449af42aae5b
Bishal487 <bishal.kunwar487@gmail.com>
2024-04-03 22:18:35 -0400

    array shift method

---
+++ Arrays/shiftMethod.ts
@@ -0,0 +1,9 @@
+let shift1:number[] = [12, 23, 24, 25, 26];
+let shift2 = shift1.shift();
+
+console.log(shift2);
+console.log(shift1);
+
+let shift3 = shift1.shift();
+console.log(shift3);
+console.log(shift1);
