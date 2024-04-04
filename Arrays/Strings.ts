5f7d2c7af8b85a1a6340eb0290f56f4f12d6b52b
Bishal487 <bishal.kunwar487@gmail.com>
2024-04-04 14:59:53 -0400

    a small note with strings in TS

---
+++ Arrays/Strings.ts
@@ -0,0 +1,25 @@
+// // In TypeScript, the string is sequence of char values and also considered as an object. It is a type of primitive data type that is used to store text data. The string values are used between single quotation marks or double quotation marks, and also array of characters works same as a string. TypeScript string work with the series of character.
+
+// // Some String Methods in TS or let's say in general
+
+
+// // Some String methods are as below
+// charAt(): This method will return character at the specified index.
+// charCodeAt(): This method will return returns a number indicating the Unicode value of the character at the specified index.
+// concat(): This method will combine the two separate string and return that combined string.
+// indexOf(): It will return the index. Fast occurrence of specified value else -1 if not found.
+// lastIndexOf(): It will return index, last occurrence of specified value else -1 if not found.
+// localeCompare(): This method will return a number which indicate that the reference string comes before or after or the same as the given string in sort order.
+// match(): This method is used to match regular expression with the specified string.
+// replace(): This method is use to replace the match string by regular expression.
+// search(): This method is used to search for matching the regular expression with the specified string.
+// slice(): Extract the specified string and return a new string.
+// split(): Splits the specified String object into an array of strings.
+// substr(): Return the character from start to define index.
+// substring(): Returns character of string between two define indexes.
+// toLocaleLowerCase(): This method convert the string into lowercase while respecting the current locale.
+// toLocaleUpperCase(): This method convert the string into Uppercase while respecting the current locale.
+// toLowerCase(): This method convert the string into lowercase.
+// toString(): This method returns a string representing the specified object.
+// toUpperCase(): This method convert the string into uppercase.
+// valueOf(): This method returns the primitive value of specified string.
