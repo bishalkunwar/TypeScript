04f55f415a110605d00a1a64dda34296a4520959
Bishal487 <bishal.kunwar487@gmail.com>
2024-04-04 10:46:34 -0400

    push pop shift unshift

---
+++ Arrays/popMethod.ts
@@ -0,0 +1,24 @@
+let arrToPop: number[] = [1, 2, 3, 4, 5];
+let poppedElement: number | undefined = arrToPop.pop();
+console.log("Popped Element:", poppedElement); // Outputs: Popped Element: 5
+console.log("Updated Array:", arrToPop); // Outputs: Updated Array: [1, 2, 3, 4]
+
+
+// let arrToShift: number[] = [1, 2, 3, 4, 5];
+// let shiftedElement: number | undefined = arrToShift.shift();
+// console.log("Shifted Element:", shiftedElement); // Outputs: Shifted Element: 1
+// console.log("Updated Array:", arrToShift); // Outputs: Updated Array: [2, 3, 4, 5]
+
+
+
+// // Push + unshift
+// let arrToPush: number[] = [1, 2, 3, 4, 5];
+// let newLength: number = arrToPush.push(6, 7);
+// console.log("New Length:", newLength); // Outputs: New Length: 7
+// console.log("Updated Array:", arrToPush); // Outputs: Updated Array: [1, 2, 3, 4, 5, 6, 7]
+
+
+// let arrToUnshift: number[] = [1, 2, 3, 4, 5];
+// let newLength: number = arrToUnshift.unshift(-1, 0);
+// console.log("New Length:", newLength); // Outputs: New Length: 7
+// console.log("Updated Array:", arrToUnshift); // Outputs: Updated Array: [-1, 0, 1, 2, 3, 4, 5]
