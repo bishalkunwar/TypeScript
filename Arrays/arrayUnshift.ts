1dca16d714608e6d465b8a6c39b8519a556f568a
Bishal487 <bishal.kunwar487@gmail.com>
2024-04-04 10:14:22 -0400

    array unshift method

---
+++ Arrays/arrayUnshift.ts
@@ -0,0 +1,9 @@
+// The Array.unshift() is an inbuilt TypeScript function that is used to add one or more elements to the beginning of an array and returns the new length of the array. 
+
+let orgArray:number[] = [11, 12, 13, 14, 15];
+console.log(orgArray);
+ 
+// let arrayUnshifted:number = 
+orgArray.unshift(34, 45);
+//console.log(arrayUnshifted);
+console.log(orgArray);
